28f1e9c54456319d740685fc1e4b557069b6217f
marborgna <69911186+marborgna@users.noreply.github.com>
2022-03-06 19:37:00 -0300

    Get pedidos
    Declaración endpoint GET /pedidos

--- server.js
+++ server.js
@@ -17,29 +17,86 @@ var connection = mysql.createConnection({
     password: '1234',
     database: 'my_db'
 });
+connection.connect();
 
  
-// EJEMPLOS ENDPOINTS
-
+//ENDPOINTS
 
 app.get('/pedido', (req, res) => {
+    var idPedido = req.query.idPedido;
+    var query = "SELECT id, price, creationDate, state, paymentType \
+         FROM pedido \
+         WHERE id = ?";
+         
+    connection.query(query, [idPedido], function (error, results) {
+        if (error) throw error;
+
+        if(results.length > 0){
+            pedido = results[0];
+            res.status(200).send({
+                id: pedido.id,
+                price: pedido.price,
+                description: "en un futuro habrá respuestas",
+                state: pedido.state,
+                creationDate: pedido.creationDate,                
+                paymentType: pedido.paymentType,
+                // productos: ???
+            });
+        } else {
+            res.status(400).send("No se encontro el pedido");
+        }
+    });
 });
 
 app.post('/pedido', (req, res) => {
+    var idUsuarix = req.body.idUsuarix;
 
     res.status(200).send({
-        msg: ``
+        msg: `La accion ha sido exitosa`
     });
 });
+
+app.put('/estadoPedido', (req, res) => {
+
+});
+
+app.post('/login', (req, res) => {
+
+});
+
+app.post('/sign-up', (req, res) => {
+
+});
+
+app.get('/pedidoUsuarix', (req, res) => {
+
+});
+
+app.post('/producto', (req, res) => {
+
+});
+
+app.get('/producto', (req, res) => {
+
+});
+
+app.put('/producto', (req, res) => {
+
+});
+
+app.delete('/producto', (req, res) => {
+
+});
+
+
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log(`Server listening at port ${PORT}`);
 });
 
-
-
-
-
-
-
-connection.end()
+//connection.end()
--- setup.sql
+++ setup.sql
@@ -1,26 +1,76 @@
 DROP database IF EXISTS my_db;
 CREATE database IF NOT EXISTS my_db;
+USE my_db;
 
-CREATE table IF NOT EXISTS producto (id INTEGER PRIMARY KEY, nombre TEXT, nombreCorto TEXT, precio INTEGER);
-CREATE table IF NOT EXISTS pedido (id INTEGER PRIMARY KEY, cantidad INTEGER, formaPago INTEGER, direccion TEXT, estado TEXT, pago INTEGER);
-CREATE table IF NOT EXISTS usuario (id INTEGER PRIMARY KEY, name TEXT, userName TEXT, mail TEXT, direccion TEXT, telefono INTEGER, userPassword TEXT);
+CREATE TABLE producto (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    nombre TEXT,
+    nombreCorto TEXT,
+    precio INTEGER
+);
+
+CREATE TABLE pedido (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    idUsuario INTEGER,
+    paymentType TEXT,
+    creationDate TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    direccion TEXT,
+    state TEXT,
+    price INTEGER
+);
+
+CREATE TABLE pedido_producto (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    idPedido INTEGER,
+    idProducto INTEGER,
+    cantidad INTEGER
+);
+
+CREATE TABLE usuario (
+    id INTEGER PRIMARY KEY AUTO_INCREMENT,
+    name TEXT,
+    userName TEXT,
+    mail TEXT,
+    direccion TEXT,
+    telefono INTEGER,
+    userPassword TEXT
+);
 
 
 INSERT INTO producto VALUES (1, "Hamburguesa clásica", "HamClas", 650);
-INSERT INTO producto VALUES (2, "Sandwich veggie", 520);
-INSERT INTO producto VALUES (3, "Focaccia", 410);
-INSERT INTO producto VALUES (4, "Sandwich queso", 580);
-INSERT INTO producto VALUES (5, "Lata cerveza", 250);
-INSERT INTO producto VALUES (6, "Hamburguesa con jamón", 695);
-INSERT INTO producto VALUES (7, "Hamburguesa  especial", 710);
+INSERT INTO producto VALUES (2, "Sandwich veggie", "SandVegg", 520);
+INSERT INTO producto VALUES (3, "Focaccia", "Foca", 410);
+INSERT INTO producto VALUES (4, "Sandwich queso", "SandQ", 580);
+INSERT INTO producto VALUES (5, "Lata cerveza", "Cerv", 250);
+INSERT INTO producto VALUES (6, "Hamburguesa con jamón", "HamJam", 695);
+INSERT INTO producto VALUES (7, "Hamburguesa  especial", "HamEsp", 710);
 
 
-INSERT INTO pedido VALUES (1, "1 hamClas", 600, 73, "Tarjeta Debito", "Av Siempreviva 1234", "Nuevo", 650)
+INSERT INTO pedido (id, idUsuario, paymentType, direccion, price)
+    VALUES (1, 43, "Tarjeta Debito", "Av Siempreviva 1234", 650);
+INSERT INTO pedido_producto (idPedido, idProducto, cantidad)
+    VALUES (1, 4, 2);
+
+INSERT INTO pedido (id, idUsuario, paymentType, direccion, price)
+    VALUES (2, 55, "Efectivo", "Av las Rosas 444", 550);
 
 
 
 
-SELECT estado FROM pedido
-    JOIN preferencias
-    ON pedido.id = preferencias.id
-    WHERE pedido.estado = "entregado";
+-----------------
+
+SELECT id as idPedido, state
+    FROM pedido
+    WHERE pedido.state = "entregado";
+
+SELECT pedido_producto.idProducto, cantidad
+    FROM pedido
+    JOIN pedido_producto 
+    ON pedido_producto.idProducto = pedido.id
+    WHERE pedido.id = 1;
+
+
+SELECT usuario.name, pedido.creationDate
+    FROM pedido
+    JOIN usuario
+    ON pedido.idUsuario = usuario.id
